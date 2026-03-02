b8668133de62b054e490a34b8117ff2571e99c28
keyember <dev.oudinallan@gmail.com>
2026-03-02 16:06:57 +0100

    created project with react and starting fetch pokemon api

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./Pokemons.jsx";
+import GetPokemons from "./Pokemons.jsx";
+
+function App() {
+  return (
+    <>
+      <div>
+        <GetPokemons />
+      </div>
+    </>
+  );
+}
+
+export default App;
